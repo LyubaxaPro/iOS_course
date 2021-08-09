fe91d313cc6ddc0ed9127d87c18d08946580fa29
LyubaxaPro <lyubaxova@mail.ru>
2021-08-09 17:18:43 +0300

    Update

--- Weather/Weather/Cities/TableViewCell.swift
+++ Weather/Weather/Cities/TableViewCell.swift
@@ -4,7 +4,7 @@ import PinLayout
 final class CityTableViewCell: UITableViewCell {
     private let titleLabel = UILabel()
     private let dateUpdatedLabel = UILabel()
-    private let iconImageView = UIImageView()
+    private let iconImageButton = CustomTouchAreaButton()
     private let temperatureLabel = UILabel()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -33,8 +33,9 @@ final class CityTableViewCell: UITableViewCell {
         titleLabel.font = .systemFont(ofSize: 28, weight: .semibold)
         dateUpdatedLabel.textColor = .darkGray
         temperatureLabel.font = .systemFont(ofSize: 48, weight: .medium)
+        iconImageButton.addTarget(self, action: #selector(DidTapIconImageButton), for: .touchUpInside)
         
-        [titleLabel, dateUpdatedLabel, iconImageView, temperatureLabel].forEach{
+        [titleLabel, dateUpdatedLabel, iconImageButton, temperatureLabel].forEach{
             contentView.addSubview($0)
         }
     }
@@ -58,7 +59,7 @@ final class CityTableViewCell: UITableViewCell {
             .height(20)
             .sizeToFit(.height)
         
-        iconImageView.pin
+        iconImageButton.pin
             .size(24)
             .after(of: dateUpdatedLabel)
             .marginLeft(8)
@@ -73,8 +74,33 @@ final class CityTableViewCell: UITableViewCell {
     func configure(with model: CityViewModel){
         titleLabel.text = model.title
         dateUpdatedLabel.text =  model.dateUpdated
-        iconImageView.image = UIImage(systemName: model.systemImageName)
+        iconImageButton.setImage(UIImage(systemName: model.systemImageName), for: .normal)
         temperatureLabel.text = "\(model.temperature) C"
+    }
+    
+    @objc
+    private func DidTapIconImageButton() {
+        print("hello")
     }
 }
 
+final class CustomTouchAreaButton : UIButton {
+    private var touchableArea = CGSize(width: 44, height: 44)
+    
+    convenience init(touchableArea: CGSize = CGSize(width: 44, height: 44)) {
+        self.init()
+        self.touchableArea = touchableArea
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let dWidth = touchableArea.width - bounds.width
+        let dHeight = touchableArea.height - bounds.height
+        
+        let rect = CGRect(x: (bounds.minX - dWidth) / 2,
+                          y: (bounds.minY - dHeight) / 2,
+                          width: touchableArea.width,
+                          height: touchableArea.height)
+        return rect.contains(point)
+            
+    }
+}
